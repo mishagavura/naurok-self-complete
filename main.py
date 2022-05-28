4886fb6ab9525ef6c0b71ae2ff969177b6cbc1b9
mishagavura <1mgavura@gmail.com>
2022-05-28 03:08:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from answers import *
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+
+
+driver = webdriver.Chrome(executable_path="driver/chromedriver", options=chrome_options )
+
+driver.get("https://naurok.com.ua/test/testing/aa7c6273-a0ec-406a-9067-f1bc40de7a0f")
+test_is_compelete = False
+while not test_is_compelete:
+    time.sleep(3)
+    question_test = driver.find_element(By.CLASS_NAME, "test-content-text-inner").text
+
+    print('||||||||', question_test, '||||||||')
+    
+    for item in answers_json:
+        print('||||||||',item['question'].replace('&nbsp;', ' ').strip(), '||||||||')
+        if question_test.replace('&nbsp;', ' ').strip() == item['question'].replace('</em>', '').replace('<em>', '').replace('<strong>', '').replace('</strong>', '').replace('&nbsp;', ' ').strip():
+            print(1)
+            answers_button_arr = driver.find_elements(By.CLASS_NAME, "question-option-inner-content")
+            for elem in answers_button_arr:
+                print(elem.text)
+                print(item['answer'].replace('&nbsp;', ' ').replace('</em>', '').replace('<em>', '').replace('<strong>', '').replace('</strong>', '').strip())
+                if elem.text.strip() == item['answer'].replace('</em>', '').replace('<em>', '').replace('&nbsp;', ' ').strip():
+                    elem.click()
+    time.sleep(2)
+    try:
+        submit_button = driver.find_element(By.CLASS_NAME, "test-multiquiz-save-button")
+        submit_button.click()
+    except NoSuchElementException:
+        pass
+    
+        # print(answer_button)
+
+
